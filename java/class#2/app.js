964c2988b3eeec8a5de2bf05e3e58a5cdf7c125d
Sheryaar <Sheryaarlong@gmail.com>
2024-02-01 18:59:42 +0500

    JAVA:

---
+++ java/class#2/app.js
@@ -0,0 +1,119 @@
+// var userName = 'Sheryaar'
+// alert(userName)
+
+// => Prompt <=//
+// var userName = prompt('Please Enter Your Name', 'Ex. First Name & Last Name')
+// alert(userName)
+
+
+// => If Else Statement <=//
+// var email = prompt('Please Enter Your Email Address')
+// var message = 'Please enter @ in the address'
+// if (email =='@gmail.com'){
+//     alert('Correct')
+// }
+// else(alert(message))
+
+// var gender = prompt('Please Enter Your Gender')
+// var male = 'Welcome Sir'
+// var female = 'Welcome Ma`am'
+// if(gender === 'Male'){
+//     alert(male)
+// }
+// if(gender=== 'Female'){
+//     alert(female)
+// }
+// else{
+//     alert('Invalid, Please use first capital letter')
+// }
+
+
+// => Signs <=//
+// > is greater than
+// < is less than
+// >= is greater than or equal to
+// > is less than or equal to
+
+
+
+
+//=> Examples of programs <=//
+// var language = prompt('Please Enter Your Language')
+// if (language === "javascript") {
+//     var city = prompt("Enter your city");
+//     if (city === "karachi") {
+//         var exp = prompt("Enter your experience");
+//         if (exp === "2 years") {
+//             console.log("Congratulation, you are Hired");
+//         } else {
+//             console.log("Barhe ho jao");
+//         }
+//     } else {
+//         console.log("Karachi Aaja bhai");
+//     }
+// } else {
+//     console.log("JavaScript Seekho Beta");
+// }
+
+
+//=> Examples of programs <=//
+// var langauge = prompt('Please Enter Your Language')
+// if(langauge==='python'){
+//     var city = prompt('Please Enter Your City Name')
+//     if(city==='karachi'){
+//         var exp=prompt('Please Enter Your Experience in years')
+//         if(exp==='2 years'){
+//             console.log('Kal se Ajaana')
+//         }else{
+//             console.log('Chaalo agey barho shahbash')
+//         }
+//     }else{
+//         console.log('Karachi Ajao pehle beta phir baat karengy')
+//     }
+// }else{ console.log('Python seekh kar ao pehle')
+// }
+
+
+
+//=> Ex <=//
+// var bhae= prompt('Chote bhae ko bulao')
+// if(bhae==='nahi hai'){
+//     var kaam = prompt('To Tum Phir Samaan leao')
+//     if(kaam==='kya'){
+//         var sahi = prompt('adha kilo doodh')
+//         if(sahi==='theek hai'){
+//             console.log('Shahbash')
+//         }else{
+//             console.log('kuch aur dekhlena')
+//         }
+//     }else{
+//         console.log('dekho laana to parega')
+//     }
+
+// }else{
+//     console.log('To dhoond kar lao phir')
+// }
+
+
+//=>Ex <=//
+// var kaam = prompt('Kya lekar aana hai')
+// if(kaam==='doodh'){
+//     console.log('Leaao')
+// }
+// else if(kaam==='dahi'){
+//     console.log('Chalo leao fatafat')
+// }
+// else if(kaam==='bread'){
+//     console.log('Arey bhae leaao kuch bhi')
+// }
+// else{console.log('Jo milraha wohi lelo')}
+
+//=>Ex <=//
+// var num = prompt('Please Enter Your Age')
+// if(num<=18){
+//     console.log('Not Eligible 18+')
+// }
+// else if(num=>18){
+//     console.log('Eligible')
+// }
+// else{console.log('Only Integers allowed')}

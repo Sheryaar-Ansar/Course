c9baf8f655629d772d1295e3d86852bd6c577eab
Sheryaar <Sheryaarlong@gmail.com>
2024-02-21 15:54:39 +0500

    Js Class#6

---
+++ java/class#6/app.js
@@ -0,0 +1,9 @@
+// date methods
+
+// console.log(new Date('2000 01 12'))
+// var date = new Date('29 june 2023')
+// var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+// console.log(days[date.getDay()])
+
+// Stopwatch - 
+// I've Completed in Assignments folder named 'StopWatch'//

ed3192c485674f4fd98f29c0ef6b0243997323c5
Sheryaar <Sheryaarlong@gmail.com>
2024-02-21 15:53:44 +0500

    Js Class#3

---
+++ java/class#3/app.js
@@ -0,0 +1,43 @@
+var students=['Ahmed', 'Ali', 'Asif']
+// Array
+// This method is used for calling their names by length.
+
+// students=[]
+// students[students.length]='Ahmed'
+// students[students.length]='Ali'
+// students[students.length]='Asif'
+// console.log(students)
+
+// POP (will delete last value / name written in ' ')
+// students.pop()
+// console.log(students)
+
+// PUSH (will Add last value / name written in ' ')
+// students.push('abc', 'xyz')
+// console.log(students)
+
+// SHIFT (will Delete first value / name written in ' ')
+// students.shift()
+// console.log(students)
+
+// UNSHIFT (will Add first value / name written in ' ')
+// students.unshift('ABC', 'XYZ')
+// console.log(students)
+
+// adding names in array variable
+// students[3]='Talha'
+// students[4]='Younus'
+// students[5]='Anjum'
+// // In Array, number in index is start from 0 i.e 0, 1, 2, 3, 4, 5 || in length, it is start from 1 i.e 1,2,3,4,5,6
+// console.log(students)
+
+// SPLICE (will delete value which will be given in index i.e if we have to delete the first number we have to call it out from index i.e 0 rather than 1)
+// students.splice(1,2)
+// // It is also used to add names
+// students.splice(1,2, 'ABZ', 'XYC')
+// console.log(students)
+
+// SLICE (will copy the array)
+// var copyArr=students.slice(2,3)  //in () we have to mention the startindex , endindex number for copying array
+// console.log(students)
+// console.log(copyArr)

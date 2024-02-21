1576c58bc571783c5f98a9418a14ea05574b1603
Sheryaar <Sheryaarlong@gmail.com>
2024-02-21 15:53:56 +0500

    Js Class#4

---
+++ java/class#4/app.js
@@ -0,0 +1,62 @@
+
+// Creating function which is not default method.
+// There are two types of functions
+
+// NORMAL FUNCTION
+// function func(){
+//     var abc='Ahmed'
+//     console.log('NAME =>',abc)
+// }
+// func()
+
+// ARROW FUNCTION (ES6) Commonly used in java by developers
+// const func = () =>{
+//     var name = 'Ahmed'
+//     console.log('Name :',name)
+// }
+// func()
+
+// LOOP (for)
+
+// Increment in condition(it will create loop of 100 numbers by 2 lines of code)
+// for(i=1; i<=100;i++){
+//     console.log('Loop=>',i)
+// }
+
+// var text = 'Hello Students'
+// for(i=1;i<=5;i++){
+//     console.log('Loop=>',i,text)
+// }
+
+// var cities=['Karachi','Islamabad','Lahore','Sahil','Peshawar','Punjab']
+// for(i=0;i < cities.length;i++){ //It will write cities line by line i.e in loop 
+//     console.log(cities[i])
+// }
+
+// TABLE using loop
+// var table = 2
+// for(i=1;i<=10;i++){
+//     console.log(table + ' x '+i+' = '+table*i)
+// }
+
+// If else statement in loop
+// var cities = ['karachi', 'sukkur', 'multan', 'faisalabad']
+// var flag = false
+// for(i=0; i < cities.length; i++){
+//     if(cities[i]==='multan'){
+//         flag=true
+//     }
+// }
+// if(flag){
+//     console.log('Found')
+// }else{
+//     console.log('Not Found')}
+
+
+// BREAK (will be used to stop the loop on the mentioned value)
+// for ( i=0; i<=10; i++){
+//     console.log(i);
+//     if(i===4){
+//         break;
+//     }
+// }

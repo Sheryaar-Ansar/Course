30bba470a5717100facba70c5ef870967378aad0
Sheryaar <Sheryaarlong@gmail.com>
2024-02-21 17:59:48 +0500

    Js Class #7

---
+++ java/class#7/app.js
@@ -0,0 +1,89 @@
+
+
+//Switch statements
+// var day = 'Monday'
+// switch(day){
+//     case 'Tuesday':
+//         console.log('Today is Tuesday')
+//         break;
+//     case 'Wednesday':
+//         console.log('Today is Wednesday')
+//         break;
+//     default:
+//         console.log('Today is neither Tuesday OR Wednesday')
+// }
+
+// Ternary Operators
+// var age = 15;
+// var message =
+//         age<=18 ? 'You`re Not Eligible' : 'You`re Hired';
+// console.log(message)
+
+//Handling Events in DOM
+// var element = document.getElementById('my-element');
+// element.addEventListener('click', function(){
+//     console.log('Element Is Clicked!')
+// })
+// element.addEventListener('keydown', function(event){
+//     console.log("Keypressed:" + event.key)
+// })
+
+// Setting Timeout / Delay
+// function abc(){
+//     console.log('One')
+// }
+// console.log('Two')
+// setTimeout(abc, 5000)
+// console.log('Three')
+
+// See More / See Less Expansion of text using if / else and function statement
+// function expansionText() {
+//     var shortText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ullam necessitatibus'
+//     var text = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime vitae, provident fugiat quibusdam sunt eos asperiores ut. Itaque labore quam incidunt vero repudiandae, aspernatur ipsa molestias, sequi consequatur voluptatum eligendi!'
+
+//     var para = document.getElementById('para')
+//     var link = document.getElementById('link')
+//     if (link.innerHTML === 'See More') {
+//         para.innerHTML = text;
+//         link.innerHTML = 'See Less'
+//     } else {
+//         para.innerHTML = shortText;
+//         link.innerHTML = 'See More'
+//     }
+// }
+
+// Promise Term
+// let promise = new Promise(function(resolve, reject){
+//     var x = 'abc@gmail.com'
+//     var y = 'abc@gmail.com'
+//     if(x===y){
+//         resolve()
+//     }else{
+//         reject()
+//     }
+// })
+// promise
+// .then(function(){
+//     console.log('You`re Login Successfully')
+
+// })
+// .catch(function(){
+//     console.log('Error 404 has occurred')
+// })
+
+// Callback
+// function add(a, b, Callback){
+//     console.log(`The Sum Of ${a} and ${b} is ${a + b}`);
+//     Callback()
+// }
+// function display(){
+//     console.log('This is the sum of two numbers')
+// }
+
+// add(5,6,display)
+
+
+
+
+
+
